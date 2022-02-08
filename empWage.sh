8d5556772206ba33895374d61f7569b1c598f76b
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-02-08 23:05:02 +0530

    Added : Calculating wage for a month

--- empWage.sh
+++ empWage.sh
@@ -1,17 +1,28 @@
-#!/bin/bash -x
+#!/bin/bash
 
 empRatePerHr=20;
 isFullTime=1;
 isPartTime=2;
+maxWorkingDays=20;
+totalSalary=0;
 
-empCheck=$((RANDOM%3))
-case $empCheck in
-  $isFullTime) empHrs=8
-          ;;
-  $isPartTime) empHrs=4
-          ;;
-        *) empHrs=0
-          ;;
-esac
-salary=$(($empRatePerHr*$empHrs))
-echo $salary;
+# for loop for 20 days salary calculation
+for (( days=1; days<=$maxWorkingDays; days++))
+do
+
+	#check fulltime or part time
+   empCheck=$((RANDOM%3))
+   case $empCheck in
+      $isFullTime) empHrs=8
+                  ;;
+      $isPartTime) empHrs=4
+                  ;;
+               *) empHrs=0
+                  ;;
+   esac
+
+   salary=$(($empRatePerHr*$empHrs))
+   totalSalary=$(($totalSalary+$salary))
+done
+
+echo $totalSalary;
